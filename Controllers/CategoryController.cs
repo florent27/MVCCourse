7c0c6c85758675bdcd62a800e60fb60507dae46a
f.benech <customer.support@eslaser.com>
2022-02-25 16:58:08 +0100

    Edit section

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -57,5 +57,40 @@ namespace BulkyDonetCore30.Controllers
             }
             return View(obj);
         }
+
+        // Get
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var lCategory = mDb.Categories.Find(id);
+            //var lCategorye = mDb.Categories.FirstOrDefault(i => i.Id == id);
+            if (lCategory == null)
+            {
+                return NotFound();
+            }
+            return View(lCategory);
+        }
+
+        // Post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "Cannot Match Name");
+            }
+            if (ModelState.IsValid)
+            {
+                mDb.Categories.Add(obj);
+                // Post to db
+                mDb.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
     }
 }
--- Models/Category.cs
+++ Models/Category.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
@@ -10,10 +11,14 @@ namespace BulkyDonetCore30.Models
     {
         [Key]
         public int Id { get; set; }
+
         [Required]
         public string Name { get; set; }
 
+        [DisplayName("Display Order")]
+        [Range(1, 100, ErrorMessage = "Must be between 1 and 100")]
         public int DisplayOrder { get; set; }
+
         public DateTime CreatedDateTime { get; set; } = DateTime.Now;
     }
 }
